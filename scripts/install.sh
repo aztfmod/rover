d8aeba09e9113440e1c2c0c4736013ec508e1fec
Laurent <laurent.lesle@microsoft.com>
2019-10-13 19:44:42 +0800

    adding make local pre-req steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,17 @@
 set -e
 ./scripts/pre_requisites.sh
 
+# For make local it is required to run "make setup_dev_gitssh" or "make setup_dev_githttp"
+if [ $1 == "local" ]; then
+    pwd
+    if [ -d "../level0" ]; then
+        echo "development environment already setup"
+    else
+        echo "You need to run first 'make setup_dev_gitssh' or 'make setup_dev_githttp'"
+        exit 1
+    fi
+fi
+
 echo "loading landingzones from $1"
 echo ""
 
