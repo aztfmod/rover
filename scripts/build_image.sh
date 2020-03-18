d8459590ffd6130a9dc894f9b0d8366331972372
lolorol <lalesle@microsoft.com>
2020-03-18 08:08:36 +0000

    Adding private build support

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -10,18 +10,33 @@ echo "Creating version ${tag}"
 # Build the rover base image
 sudo docker-compose build
 
-sudo docker tag rover_rover aztfmod/rover:$tag
-sudo docker tag rover_rover aztfmod/rover:latest
 
-if [ "$1" == 'github' ]; then
-    sudo docker push aztfmod/rover:$tag
-    sudo docker push aztfmod/rover:latest
+case "$1" in 
+    "github")
+        sudo docker tag rover_rover aztfmod/rover:$tag
+        sudo docker tag rover_rover aztfmod/rover:latest
 
-    # tag the git branch and push
-    git tag $tag master
-    git push --follow-tags
-else
-    echo "Local version created"
-fi
+        sudo docker push aztfmod/rover:$tag
+        sudo docker push aztfmod/rover:latest
 
-echo "Version aztfmod/rover:${tag} created."
+        # tag the git branch and push
+        git tag $tag master
+        git push --follow-tags
+        echo "Version aztfmod/rover:${tag} created."
+        ;;
+    "private")
+        sudo docker tag rover_rover aztfmod/roverdev:$tag
+        sudo docker tag rover_rover aztfmod/roverdev:latest
+
+        sudo docker push aztfmod/roverdev:$tag
+        sudo docker push aztfmod/roverdev:latest
+        echo "Version aztfmod/roverdev:${tag} created."
+        echo "Version aztfmod/roverdev:latest created."
+        ;;
+    *)    
+        sudo docker tag rover_rover aztfmod/rover:$tag
+        sudo docker tag rover_rover aztfmod/rover:latest
+        echo "Local version created"
+        echo "Version aztfmod/rover:${tag} created."
+        ;;
+esac
