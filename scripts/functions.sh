16a1f43201418a3c9af71955018faf90780ce898
lolorol <LaurentLesle@users.noreply.github.com>
2022-06-07 04:05:28 +0000

    Update rover version check

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -864,7 +864,7 @@ function verify_rover_version {
     user=$(whoami)
 
     if [ "${ROVER_RUNNER}" = false ]; then
-        required_version=$(cat /tf/caf/.devcontainer/docker-compose.yml | yq | jq -r '.services | first(.[]).image' | awk -F'/' '{print $NF}')
+        required_version=$(cat /tf/caf/.devcontainer/docker-compose.yml | yq | jq -r '.services | first(.[]).image')
         running_version=$(cat ${script_path}/version.txt |  egrep -o '[^\/]+$')
 
         if [ "${required_version}" != "${TF_VAR_rover_version}" ]; then
