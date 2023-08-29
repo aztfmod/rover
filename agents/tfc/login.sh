a87bef23bb416e30dbb518a3367dd74cc7583419
LaurentLesle <laurent.lesle@gmail.com>
2023-08-29 07:50:44 +0000

    Add credential check for pre-hook

--- agents/tfc/login.sh
+++ agents/tfc/login.sh
@@ -3,6 +3,9 @@
 if [[ -v ARM_CLIENT_SECRET ]]; then
   echo "Logging with the service principal secret flow. ($ARM_CLIENT_ID)"
   az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID --allow-no-subscriptions >/dev/null >&1
+else
+  echo "No valid credentials provided. Only ARM_CLIENT_SECRET is supported."
+  exit 1
 fi
 
 if [[ -v ARM_SUBSCRIPTION_ID ]]; then
